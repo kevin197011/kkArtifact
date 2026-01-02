8f70346a4a98c4a776c28b1b1be1d9d25464b2b1
kk <kevin197011@outlook.com>
2026-01-02 15:20:01 +0800

    chore(ci): 项目维护
    变更文件:
      📝 .kkartifact.yml
      📝 docker-compose.prod.yml
      📝 pull.sh
      📝 push.sh
      📝 web-ui/nginx.conf

--- pull.sh
+++ pull.sh
@@ -1,6 +1,6 @@
 ./bin/kkartifact-agent pull \
-  --project video \
-  --app srs \
+  --project devops \
+  --app app01 \
   --version test-1766992486 \
   --path /tmp/srs \
   --config .kkartifact.yml
--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 ./bin/kkArtifact-agent push \
-  --project myproject \
-  --app myapp4 \
+  --project devops \
+  --app app01 \
   --version test-$(date +%s) \
   --path ./web-ui \
   --config .kkartifact.yml
